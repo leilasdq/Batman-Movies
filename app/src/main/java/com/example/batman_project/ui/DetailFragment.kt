72cc922e158c9a3dba872c6f26acaaf0bcb1d27c
leila <l.sedighi.2015@gmail.com>
2020-06-30 12:13:14 +0430

    Edit ui for DetailFragment.

---
+++ app/src/main/java/com/example/batman_project/ui/DetailFragment.kt
@@ -0,0 +1,24 @@
+package com.example.batman_project.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.batman_project.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class DetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+}
